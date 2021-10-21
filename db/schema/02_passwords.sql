ccfd1db48919581318ac9e4bd0b54240c4637353
Matthew Parisien <matthewparisien4@gmail.com>
2021-10-21 17:24:06 +0000

    Add schema for password entries

--- db/schema/02_passwords.sql
+++ db/schema/02_passwords.sql
@@ -2,10 +2,11 @@ DROP TABLE IF EXISTS passwords CASCADE;
 
 CREATE TABLE passwords (
   id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
   url VARCHAR(255) NOT NULL,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  category,
+  category ,
   note TEXT,
   user_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
   org_id INTEGER REFERENCES organizations(id) ON DELETE CASCADE,
