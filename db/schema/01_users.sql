5dbf61d3bf7e41022d8b5b60b63ecdbf0031be0b
bilykb <brettbilyk@gmail.com>
2021-10-21 01:23:13 +0000

    Add database schema for table accounts As per the ERD, this tables includes the columns "id, email, main_password, created, and active"

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,9 @@
--- Drop and recreate Users table (Example)
+DROP TABLE IF EXISTS accounts CASCADE;
 
-DROP TABLE IF EXISTS users CASCADE;
-CREATE TABLE users (
+CREATE TABLE accounts (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  main_password VARCHAR(255) NOT NULL
+  created DATE NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE
 );
