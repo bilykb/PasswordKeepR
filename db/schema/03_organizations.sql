523eedb29687215963ac421f2c0a634679fcf3a6
bilykb <brettbilyk@gmail.com>
2021-10-21 19:35:06 +0000

    Remove some properties that will be considered stretch goals.  Add additional properties. Changes reflect a more focused MVP

--- db/schema/03_organizations.sql
+++ db/schema/03_organizations.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS organizations CASCADE;
 
 CREATE TABLE organizations (
   id SERIAL PRIMARY KEY NOT NULL,
-  legal_name VARCHAR(255) NOT NULL,
-  date_deactivated DATETIME NOT NULL,
-  active BOOLEAN NOT NULL DEFAULT TRUE
+  name VARCHAR(255) NOT NULL,
+  created DATETIME NOT NULL,
+  updated DATETIME NOT NULL
 );
