566e908612cbaa54a5cefd4faf19af88be103e7e
bilykb <brettbilyk@gmail.com>
2021-10-22 00:14:50 +0000

    Forgot a thing

--- db/seeds/02_passwords.sql
+++ db/seeds/02_passwords.sql
@@ -7,5 +7,5 @@ VALUES ('Google', 'www.google.ca', 'oogle', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/
 ('instagram', 'www.instagram.com', 'Yello', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 3, 3),
 ('youtube', 'www.youtube.com', 'lookAtMe', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 2, 7),
 ('banking', 'www.atb.com', 'myName', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 1, 8,),
-('power', 'www.enmax.com', 'someName', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 1, 6, 2)
+('power', 'www.enmax.com', 'someName', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 1, 6, 2),
 ('CBC', 'www.cbc.ca', 'yesIam', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', 4, 1);
