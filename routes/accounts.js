e1d6b0641f6fa4fab23ae4e09205560f4d510389
Matthew Parisien <matthewparisien4@gmail.com>
2021-10-22 20:48:32 +0000

    Create new banch to build log in GET & POST routes

--- routes/accounts.js
+++ routes/accounts.js
@@ -1,35 +1,22 @@
-const bcrypt = require('bcryptjs');
-const express = require('express');
+const bcrypt = require("bcryptjs");
+const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-
   // get registration page
-  router.get('/register'), (req, res) => {
-
-  };
+  router.get("/register"), (req, res) => {};
 
   // get login form
-  router.get('/login'), (req, res) => {
-
-  };
+  router.get("/login"), (req, res) => {};
 
   // register a new user
-  router.post('/register', (req, res) => {
-
-  });
+  router.post("/register", (req, res) => {});
 
   // login (will need a login function)
-  router.post('/login', (req, res) => {
-
-  });
+  router.post("/login", (req, res) => {});
 
   // logout
-  router.post('/logout', (req, res) => {
-
-  })
+  router.post("/logout", (req, res) => {});
 
   return router;
-}
-
-
+};
