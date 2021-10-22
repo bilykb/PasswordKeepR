a700a2046e8903bd4470bf0f1d7841df81d51fa0
bilykb <brettbilyk@gmail.com>
2021-10-22 20:38:57 +0000

    Add skeleton for passwords routes

---
+++ routes/passwords.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+
+  //Get list of passwords dashboard
+  router.get('/passwords'), (req, res) => {
+
+  };
+
+  //Get create a new password form
+  router.get('/passwords/new', (req, res) => {
+
+  });
+
+  //Create a new password
+  router.post('/passwords', (req, res) => {
+
+  });
+
+  //Get create a new password form
+  router.get('/passwords/id/:edit', (req, res) => {
+
+  });
+
+  //Update a password
+  router.post('passwords/:id', (req, res) => {
+
+  });
+
+  //Delete a password
+  router.post('passwords/:id', (req, res) => {
+
+  });
+
+  return router;
+}
