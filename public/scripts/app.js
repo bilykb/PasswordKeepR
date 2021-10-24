482b359a556b76a6a6deb7730d266f083d4f09f0
Matthew Parisien <matthewparisien4@gmail.com>
2021-10-24 20:45:39 +0000

    Spacing changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,14 +4,13 @@ $(() => {
 
   $(".edit_btn").on("click", function(e) {
     const $container =  $(".edit_password_container");
-    $container.fadeIn(200);
+    $container.addClass("in_view");
     const $formFields = $container.find("input, select");
 
     //This request will be modularized later on
     $.post('/api/passwords', (data) => {
       console.log(data)
     })
-
 
     $formFields.each((i, field) => {
       $(field).val('this will be the current value');
@@ -21,7 +20,7 @@ $(() => {
   //Close edit form when cancel button is clicked
   $(".cancel_btn").on("click", function() {
     //Reset to hidden state
-    $($(this).parents()[2]).fadeOut(200);
+    $($(this).parents()[2]).removeClass("in_view");
   })
 })
 
