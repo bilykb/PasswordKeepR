d8d720a828cd7be65dc1b7ec7d76d9cfd87a1058
bilykb <brettbilyk@gmail.com>
2021-10-26 23:49:14 +0000

    Edit cancel button event listeners

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,13 +10,14 @@ $(() => {
   //Close edit form when cancel button is clicked
   $(".cancel_btn").on("click", function() {
     //Reset to hidden state
-    $($(this).parents()[3]).removeClass("in_view");
+    $(this).closest(".edit_password_container").removeClass("in_view");
+    $(this).closest(".create_new_password_container").removeClass("in_view");
   })
 
   //Shows the add passord form when the + button is pressed
   $(".create_new_password").on("click", function() {
     //Un hiddens form
-    const $container =  $(".create_new_password_container");
+    const $container = $(".create_new_password_container");
     $container.addClass("in_view");
     const $formFields = $container.find("input, select");
   })
