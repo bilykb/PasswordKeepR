0a1664fb7d805161ed073f2b911fc0d745cf124d
Matthew Parisien <matthewparisien4@gmail.com>
2021-10-26 21:46:53 +0000

    Implement new layout struture - to confirm with partners later

---
+++ public/scripts/password_list.js
@@ -0,0 +1,11 @@
+//Scripts for password list
+
+$(() => {
+
+  $(".hidden").mouseenter(function() {
+    $(this).css("opacity", "1");
+  }).mouseleave( function() {
+    $(this).css("opacity", "0");
+  });
+
+})
