77ddbc3f34cb386af2184163545a5ba81cd31a7b
Matthew Parisien <matthewparisien4@gmail.com>
2021-10-27 16:39:10 +0000

    Implement responsive design

--- public/scripts/anim.js
+++ public/scripts/anim.js
@@ -1,11 +1,8 @@
 /******  LAYOUT ANIMATIONS *******/
+const tl = gsap.timeline();
 
-$(() => {
 
   const introAnimation = function() {
-
-    const tl = gsap.timeline();
-
     const toAnimate = [];
     const heading1 = toAnimate.push($('.personal_passwords_container h2')[0]);
     const privateList = toAnimate.push($($('.password_list')[0]).find("li"));
@@ -18,7 +15,9 @@ $(() => {
     }, {
       x: 0,
       duration: 2,
-      ease:'Expo.easeOut'
+      ease:'Expo.easeOut',
+
+
     });
     tl.fromTo(toAnimate, {
       opacity: 0,
@@ -28,7 +27,8 @@ $(() => {
       y: 0,
       stagger: 0.2,
       duration: 3,
-      ease:'Expo.easeOut'
+      ease:'Expo.easeOut',
+
     }, 1);
     tl.fromTo('.create_new_password', {
       opacity: 0,
@@ -37,7 +37,8 @@ $(() => {
       opacity: 1,
       y: 0,
       duration: 2,
-      ease: 'Expo.easeOut'
+      ease: 'Expo.easeOut',
+
     }, 2);
     tl.fromTo('nav h1, .nav_right, .dropdown', {
       opacity: 0,
@@ -46,14 +47,21 @@ $(() => {
       opacity: 1,
       y: 0,
       duration: 2,
-      ease: 'Expo.easeOut'
+      ease: 'Expo.easeOut',
+
     }, 3)
   }
 
-  if (!sessionStorage.getItem("isVisited")) {
-    introAnimation();
-  };
 
-  sessionStorage.setItem('isVisited', 'true');
-})
+    if (!sessionStorage.getItem("isVisited")) {
+      console.log('in here1')
+      introAnimation();
+    };
+    sessionStorage.setItem('isVisited', 'true');
+
+
+
+
+
+
 
